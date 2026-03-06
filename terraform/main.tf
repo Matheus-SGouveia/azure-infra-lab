39e091494d444efacb1548178bcd215a54ad9060
Matheus-SGouveia <matheussg58@hotmail.com>
2026-03-06 17:03:04 -0300

    Atualização: ocultação de informações sensíveis

--- terraform/main.tf
+++ terraform/main.tf
@@ -89,7 +89,8 @@ resource "azurerm_linux_virtual_machine" "vm" {
 
   admin_ssh_key {
     username   = "azureuser"
-    public_key = file("/home/usuário/Downloads/azure-infra-lab-rsa.pub")
+    public_key = file("/home/usuario/Downloads/azure-infra-lab-rsa.pub")
+
   }
 
   os_disk {
